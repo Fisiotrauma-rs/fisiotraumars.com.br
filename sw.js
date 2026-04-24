83782f301b121007804645c7e6a105e49f7366ca
Fisiotrauma-rs <fisiotrauma-rs@hotmail.com>
2026-04-23 21:38:40 -0300

    Implement service worker for caching assets

--- sw.js
+++ sw.js
@@ -1,1 +1,40 @@
+const CACHE_NAME = 'fisiotraumars-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/sobre.html',
+  '/servicos.html',
+  '/contato.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
+];
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
